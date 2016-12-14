f0b146193e05d268941726f7366b4adee6300a1b
tjabbe <martin.goutry@gmail.com>
2016-12-14 04:29:52 +0100

    feat(activities): created class for remaining activities

---
+++ app/src/main/java/com/example/martin/captainlunch/CreateTrain.java
@@ -0,0 +1,24 @@
+package com.example.martin.captainlunch;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+/**
+ * Created by Martin on 13/12/2016.
+ */
+
+public class CreateTrain extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
