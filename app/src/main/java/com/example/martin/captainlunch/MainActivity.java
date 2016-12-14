b135595653f25a617e9f8cac14995de3d9500259
tjabbe <martin.goutry@gmail.com>
2016-12-14 04:25:50 +0100

    feat(main activity): update main activity

--- app/src/main/java/com/example/martin/captainlunch/MainActivity.java
+++ app/src/main/java/com/example/martin/captainlunch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.martin.captainlunch;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
