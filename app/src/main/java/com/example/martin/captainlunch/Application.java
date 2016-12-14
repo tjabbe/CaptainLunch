e06392f71c85fb8123f8f5fdbf0dfcbf1f1d5277
tjabbe <martin.goutry@gmail.com>
2016-12-14 04:20:59 +0100

    feat(style): set up font manager

---
+++ app/src/main/java/com/example/martin/captainlunch/Application.java
@@ -0,0 +1,22 @@
+package com.example.martin.captainlunch;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by Martin on 13/12/2016.
+ */
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // init Calligraphy for easier fonts management
+        CalligraphyConfig.initDefault(
+                new CalligraphyConfig.Builder()
+                    .setDefaultFontPath("fonts/geomanist-regular.ttf")
+                    .setFontAttrId(R.attr.fontPath)
+                    .build()
+        );
+    }
+}
